08a290e1f002ca87219820122b1b41af42c7b0a4
diego <diegopgcds10@gmail.com>
2022-01-14 13:43:09 -0300

    Configurando o Redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import PageHeader from "../template/pageHeader"
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Muito boa...</p>
+        <h2>Missão e Visão</h2>
+        <p>Acredito em Deus...</p>
+        <h2>Imprensa</h2>
+        <p>Lugar para espalhar notícias...</p>
+    </div>
+)
